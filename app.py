55a09db0f48375a9954bed3ef3afcfb61763ec56
Ali Farsi <ali.farsi.nejad@gmail.com>
2020-06-29 12:43:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
 
+from flask import Flask, request
+from bert_extractive_summary import *
+
+
+
+
+app = Flask("__main__")
+
+@app.route("/")
+def home():
+    return " working"
+
+
+@app.route("/api/summarizer", methods=["GET", "POST"])
+def api():
+    video_id = request.args["video_id"]
+    text = main(video_id)
+    return text
+if __name__ == "__main__":
+    app.run()
